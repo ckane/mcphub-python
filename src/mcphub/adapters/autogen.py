8b9cee3f634697fd049d6a2fa290fedd05c9ae66
Hieu Tran <hieutrantrung.it@gmail.com>
2025-04-14 21:02:16 +0700

    [hot-fix] autogen create_adapters and add examples

--- src/mcphub/adapters/autogen.py
+++ src/mcphub/adapters/autogen.py
@@ -1,17 +1,25 @@
 try:
     from typing import List
 
-    from autogen_ext.tools.mcp import StdioMcpToolAdapter
+    from autogen_ext.tools.mcp import StdioMcpToolAdapter, StdioServerParams
 
     from .base import MCPBaseAdapter
 
     class MCPAutogenAdapter(MCPBaseAdapter):
         async def create_adapters(self, mcp_name: str) -> List[StdioMcpToolAdapter]:
             server_params = self.get_server_params(mcp_name)
+            
+            autogen_mcp_server_params = StdioServerParams(
+                command=server_params.command,
+                args=server_params.args,
+                env=server_params.env,
+                cwd=server_params.cwd
+            )
+
             async with self.create_session(mcp_name) as session:
                 tools = await session.list_tools()
                 return [
-                    await StdioMcpToolAdapter.from_server_params(server_params, tool.name)
+                    await StdioMcpToolAdapter.from_server_params(autogen_mcp_server_params, tool.name)
                     for tool in tools.tools
                 ]
                 
